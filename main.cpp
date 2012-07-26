fbd1de1fdda197deda6f5fcca976aded65240629
Leonardo Galteri <leonardogalteri@gmail.com>
2012-07-26 03:42:49 +0200

    Qualche prima dichiarazione nel main...

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-
+#include "player.h"
+#include "cake.h"
 int main(int argc, char **argv) 
 {
+    Player player_a, player_b;
+    Cake cake;
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
