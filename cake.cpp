2e1099c39e935536ba412223aa8e91d2d8dff37a
Leonardo Galteri <leonardogalteri@gmail.com>
2012-07-26 03:38:56 +0200

    Aggiunta classi player e cake

---
+++ cake.cpp
@@ -0,0 +1,12 @@
+#include "cake.h"
+
+Cake::Cake()
+{
+
+}
+
+Cake::~Cake()
+{
+
+}
+
---
+++ cake.h
@@ -0,0 +1,12 @@
+#ifndef CAKE_H
+#define CAKE_H
+
+class Cake
+{
+
+public:
+    Cake();
+    virtual ~Cake();
+};
+
+#endif // CAKE_H
